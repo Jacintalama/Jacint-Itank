72cbefeedcac7bb79ea78468a6499f53d087bb84
John Lester Regalado <janjanregalado1@gmail.com>
2024-02-16 08:26:52 +0800

    lester another commit

--- iTANK/src/components/Navbar.tsx
+++ iTANK/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const NavBar: React.FC = () => {
   return (
     <nav className="flex items-center justify-center py-4 bg-white shadow-md">
-      <span className="text-black font-bold text-xl">Water District of General Santos City</span>
+      <span className="text-black font-bold text-xl">iTANK</span>
     </nav>
   );
 };
